c0199e4944f5a07ae0f72dfd9377eadc3ef3b6bb
Ubiratan S. Freitas <usfreitas@gmail.com>
2025-04-10 16:38:28 +0200

    Adjust license identifier

--- operators.py
+++ operators.py
@@ -1,21 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2023 Ubiratan S. Freitas
-
-
-# This file is part of Lattesh.
-# 
-# Lattesh is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License as published by the 
-# Free Software Foundation, either version 3 of the License, or (at your option) 
-# any later version.
-# 
-# Lattesh is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
-# more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
-# Lattice Tools. If not, see <https://www.gnu.org/licenses/>. 
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 cirp GmbH
+# SPDX-FileContributor: Ubiratan S. Freitas
 
 
 
